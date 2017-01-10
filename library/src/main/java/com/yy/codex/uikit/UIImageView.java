66a47a06fc6791180de7402194abf640bc2e2056
PonyCui <cuis@vip.qq.com>
2017-01-10 16:18:57 +0800

    Add UIImageView animationImages.

--- library/src/main/java/com/yy/codex/uikit/UIImageView.java
+++ library/src/main/java/com/yy/codex/uikit/UIImageView.java
@@ -1,7 +1,10 @@
 package com.yy.codex.uikit;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.os.Build;
+import android.os.Looper;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
@@ -47,6 +50,8 @@ public class UIImageView extends UIView {
 
     /* UIImageView Props */
 
+    /* Image Source */
+
     private UIImage image;
 
     public UIImage getImage() {
@@ -54,9 +59,15 @@ public class UIImageView extends UIView {
     }
 
     public void setImage(UIImage image) {
+        if (this.image == image) {
+            return;
+        }
         this.image = image;
         this.getLayer().setBitmap(image.getBitmap());
+        invalidate();
     }
+
+    /* Content Mode */
 
     private UIViewContentMode contentMode = UIViewContentMode.ScaleToFill;
 
@@ -104,4 +115,71 @@ public class UIImageView extends UIView {
         }
     }
 
+    /* Animated Image */
+
+    private ValueAnimator animator = null;
+    private UIImage[] animationImages = null;   // The array must contain UIImages. Setting hides the single image. default is nil
+    private double animationDuration = 0.0;     // for one cycle of images. default is number of images * 1/30th of a second (i.e. 30 fps)
+    private int animationRepeatCount = 0;       // 0 means infinite (default is 0)
+
+    public UIImage[] getAnimationImages() {
+        return animationImages;
+    }
+
+    public void setAnimationImages(UIImage[] animationImages) {
+        stopAnimating();
+        this.animationImages = animationImages;
+        this.animationDuration = this.animationImages.length * (1.0 / 30.0);
+    }
+
+    public double getAnimationDuration() {
+        return animationDuration;
+    }
+
+    public void setAnimationDuration(double animationDuration) {
+        this.animationDuration = animationDuration;
+    }
+
+    public int getAnimationRepeatCount() {
+        return animationRepeatCount;
+    }
+
+    public void setAnimationRepeatCount(int animationRepeatCount) {
+        this.animationRepeatCount = animationRepeatCount;
+    }
+
+    public void startAnimating() {
+        stopAnimating();
+        if (animationImages != null && animationImages.length > 0) {
+            ValueAnimator valueAnimator = new ValueAnimator().ofInt(0, animationImages.length);
+            valueAnimator.setDuration((long)(animationDuration * 1000));
+            if (animationRepeatCount == 0) {
+                valueAnimator.setRepeatCount(99999);
+            }
+            else {
+                valueAnimator.setRepeatCount(animationRepeatCount);
+            }
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    int currentIndex = (int)valueAnimator.getAnimatedValue();
+                    if (currentIndex < animationImages.length) {
+                        setImage(animationImages[currentIndex]);
+                    }
+                }
+            });
+            valueAnimator.start();
+            this.animator = valueAnimator;
+        }
+    }
+
+    public void stopAnimating() {
+        if (this.animator != null){
+            this.animator.cancel();
+        }
+    }
+
+    public boolean isAnimating() {
+        return this.animator != null && this.animator.isRunning();
+    }
 }
--- library/src/main/java/com/yy/codex/uikit/UIViewContentMode.java
+++ library/src/main/java/com/yy/codex/uikit/UIViewContentMode.java
@@ -8,7 +8,7 @@ public enum UIViewContentMode {
     ScaleToFill,
     ScaleAspectFit,      // contents scaled to fit with fixed aspect. remainder is transparent
     ScaleAspectFill,     // contents scaled to fill with fixed aspect. some portion of content may be clipped.
-    Redraw,              // redraw on bounds change (calls -setNeedsDisplay)
+    Redraw,              // redraw on bounds change (calls -setNeedsDisplay).
     Center,              // contents remain same size. positioned adjusted.
     Top,
     Bottom,
