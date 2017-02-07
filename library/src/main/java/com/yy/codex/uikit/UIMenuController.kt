b4fb241ff589e567373b118cfc60a46d42f44b37
ponycui <PonyCui_Home@saitekiiMac.local>
2017-02-07 23:32:40 +0800

    …

--- library/src/main/java/com/yy/codex/uikit/UIMenuController.kt
+++ library/src/main/java/com/yy/codex/uikit/UIMenuController.kt
@@ -24,22 +24,33 @@ class UIMenuController {
     }
 
     fun setTargetWithRect(targetRect: CGRect, targetView: UIView) {
-
+        this.targetRect = targetRect
+        this.targetView = targetView
+        createMenuView()
     }
 
     var arrowDirection: ArrowDirection = ArrowDirection.Default
+        set(value) {
+            field = value
+            createMenuView()
+        }
 
     var menuItems: List<UIMenuItem> = listOf()
+        set(value) {
+            field = value
+            createMenuView()
+        }
 
+    private var targetRect: CGRect? = null
+    private var targetView: UIView? = null
     private var menuView: UIView? = null
     private var maskView: UIView? = null
 
-    private var context: Context? = null
-
     private fun createMenuView() {
-        context?.let {
-            menuView = UIView(it)
-            maskView = UIView(it)
+        targetView?.let {
+            menuView = UIView(it.context)
+            maskView = UIView(it.context)
+            maskView?.setBackgroundColor(UIColor.clearColor)
             maskView?.frame = CGRect(0.0, 0.0, UIScreen.mainScreen.bounds().width, UIScreen.mainScreen.bounds().height)
         }
     }
